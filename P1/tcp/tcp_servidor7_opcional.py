68517ade106da23aa1a78119ca0ed53c01aeb30a
Nahuel Costa Cortez <uo251652@uniovi.es>
2018-10-03 08:43:09 +0000

    Archivos correspondientes a la segunda parte de la practica de TCP

---
+++ P1/tcp/tcp_servidor7_opcional.py
@@ -0,0 +1,54 @@
+import socket
+import sys
+import time
+import struct
+
+
+# Creación del socket de escucha
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Podríamos haber omitido los parámetros, pues por defecto ‘socket()‘ en python
+# crea un socket de tipo TCP
+
+#asignamos puerto si lo recibe por linea de comandos
+arg = len(sys.argv)
+if arg == 2:
+    puerto = int(sys.argv[1])
+else:
+    puerto = 9999
+
+# Asignarle puerto
+s.bind(("", puerto))
+
+# Ponerlo en modo pasivo
+s.listen(5) # Máximo de clientes en la cola de espera al accept()
+
+# Bucle principal de espera por clientes
+while True:
+    print("Esperando un cliente")
+    sd, origen = s.accept()
+    time.sleep(1)
+    print("Nuevo cliente conectado desde %s, %d" % origen)
+    continuar = True
+    # Bucle de atención al cliente conectado
+    while continuar:
+        # Primero recibir el mensaje del cliente
+        mensaje = sd.recv(80)
+        longitud = struct.unpack(">H", mensaje[0:2])
+        longitud = longitud[0]
+        mensaje = mensaje[2:2+longitud]
+        mensaje = str(mensaje, "utf8")
+
+        if longitud==0: # Fin de transmision de datos por parte del cliente
+            print("Conexión cerrada por el cliente")
+            sd.close()
+            continuar = False
+
+        # Tercero, darle la vuelta
+        mensaje = mensaje[::-1]
+
+        # Finalmente, enviarle la respuesta 
+        lonbytes = struct.pack(">H", len(bytes(mensaje, "utf8")))
+        print(lonbytes + bytes(mensaje, "utf8"))
+        sd.sendall(lonbytes + bytes(mensaje, "utf8"))
+
+
