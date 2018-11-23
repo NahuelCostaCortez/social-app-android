54b0e360383b63dd28fb0951139e15eddda40859
Nahuel Costa Cortez <uo251652@uniovi.es>
2018-11-23 00:51:47 +0000

    Últimos archivos práctica 4

---
+++ P4/email_pop3_recibe_crudo.py
@@ -0,0 +1,61 @@
+import sys
+import socket
+import ssl
+import getpass
+import email.message, email.policy, email.utils
+import io
+
+def RecvReply(socket):
+    mensaje = socket.recv(1024).decode()
+    print("S: "+mensaje)
+    if not(mensaje[0:3]=="+OK"):
+       print("Error")
+       sys.exit()
+
+def RecvReply2(socket):
+    mensaje = socket.recv(1024).decode()
+    msg=mensaje
+    while("\n.\r\n" not in msg):
+       mensaje = socket.recv(1024).decode()
+       msg+=mensaje
+    buf = io.StringIO(msg)
+    while True: 
+       s=buf.readline()
+       if "From:" in s and "h=" not in s:
+          print(s)
+       if "Subject:" in s and "h=" not in s:
+         print(s)
+         break
+
+server="pop.gmail.com"
+port=995
+
+username = input("Usuario: ")
+password = getpass.getpass("Contraseña: ")
+
+s = socket.socket()
+s.connect((server, port))
+sc = ssl.wrap_socket(s)
+RecvReply(sc)
+
+mensaje = "USER "+username+"\r\n"
+print("C: "+ mensaje)
+sc.send(mensaje.encode())
+RecvReply(sc)
+
+mensaje = "PASS "+password+"\r\n"
+#print("C: "+ mensaje)
+sc.send(mensaje.encode())
+RecvReply(sc)
+
+mensaje = "STAT\r\n"
+print("C: "+ mensaje)
+sc.send(mensaje.encode())
+RecvReply(sc)
+
+mensaje = "RETR 1\r\n"
+print("C: "+ mensaje)
+sc.send(mensaje.encode())
+RecvReply2(sc)
+#recibeMensaje(sc)
+
